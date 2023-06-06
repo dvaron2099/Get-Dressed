da3f37f3e8c57fde3c4aded4e7de78300d5c2e86
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-06-06 17:18:35 -0300

    has_one y has_many attached en los modelos clothe y user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :clothes, dependent: :destroy
   has_many :rents
+  has_one_attached :photo
   # has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
