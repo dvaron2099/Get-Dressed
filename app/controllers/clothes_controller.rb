ffea2d0e33d846bd717bab38e589cbf4816a69f1
david varon <varondavid20@gmail.com>
2023-06-06 13:30:05 -0500

    parte del index hecha

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -1,2 +1,15 @@
 class ClothesController < ApplicationController
+  before_action :set_clothe, only: %i[show]
+  def index
+    @clothes = Clothe.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_clothe
+    @clothe = Clothe.find(params[:id])
+  end
 end
