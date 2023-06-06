71b2246f0e11499ed449aa4364749ffaa1bf35c9
david varon <varondavid20@gmail.com>
2023-06-06 13:48:47 -0500

    a crear el new

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -7,7 +7,25 @@ class ClothesController < ApplicationController
   def show
   end
 
+  def new
+    @clothe = Clothe.new
+  end
+
+  def create
+    @clothe = Clothe.new(clothe_params)
+    if @clothe.save
+      redirect_to clothe_path(@clothe), notice: "Prenda cargada exitosamente"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def clothe_params
+    params.require(:clothe).permit(:name, :category, :price, :size, :gender, :description)
+    # agregar fotos
+  end
 
   def set_clothe
     @clothe = Clothe.find(params[:id])
